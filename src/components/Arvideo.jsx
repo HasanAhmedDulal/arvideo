333e65002c4895e1154143621e719514653b163d
Hasan Ahmed Dulal <41692195+HasanAhmedDulal@users.noreply.github.com>
2025-07-21 03:49:21 +0600

    first commit

--- src/components/Arvideo.jsx
+++ src/components/Arvideo.jsx
@@ -105,7 +105,7 @@ export default function ArVideo() {
                         ref={videoRef}
                         autoPlay
                         loop
-
+                        muted
                         playsInline
                         style={{ display: 'none' }}
                         className='h-screen'
